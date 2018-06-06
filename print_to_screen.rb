d44a4d38eccda782bec9600a49483a655f6d539f
Boothe Davis <photoboothe@yahoo.com>
2018-06-06 12:15:13 -0400

    printed 1 to 100 and replaced 3 with mined

---
+++ print_to_screen.rb
@@ -0,0 +1,24 @@
+# p 1
+# puts 1
+# print 1
+
+# 100.times do
+#  puts 1
+# end
+
+# number = 1
+# 100.times do
+# puts number
+# number = number + 1
+# end
+
+number = 1
+100.times do
+	if number ==3
+		puts "mined"
+	else
+		puts number
+	end
+number = number + 1
+
+end
